5b0959e2d277793abab78890e30e2e01ab50ac31
Shinyanonn <y.shinya2020@icloud.com>
2024-03-11 22:22:08 +0900

    画像まで

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello torilingo</p>
-      <h1 className="text-blue-500">{`I'm Keny`}</h1>
+    <div className="text-center mt-20">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-blue-500">
+        {`ToriLingoにようこそ！`}
+      </h1>
+      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <Image
+          src="/bird_aoitori_bluebird.png"
+          width={779}
+          height={704}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
+      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <Image
+          src="/fruit_ringo.png"
+          width={758}
+          height={758}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
     </div>
   );
 }
